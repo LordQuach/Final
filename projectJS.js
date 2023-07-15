a7f3eb005b1b018d801b3daea533f3c58b092493
LordQuach <103610666+LordQuach@users.noreply.github.com>
2023-07-15 15:52:49 -0400

    inputs passed to table and placed on new page

--- projectJS.js
+++ projectJS.js
@@ -1,8 +1,72 @@
-document.getElementById("btnSubmit").addEventListener('click',plannerWindow)
+document.getElementById("btnSubmit").addEventListener('click', plannerWindow)
 function plannerWindow()
 {
-    visitorName = document.getElementById("inputName").value;
-    
-    flyWindow = window.open('about:blank','myPop','');
-    flyWindow.document.write();
+    //Form inputs
+    var visitorName = document.getElementById("inputName").value;
+    var visitorEmail = document.getElementById("inputEmail").value;
+    var visitorGoal = document.getElementById("inputGoal").value;
+    var inputM1 = document.getElementById("inputM1").value;
+    var inputM2 = document.getElementById("inputM2").value;
+    var inputM3 = document.getElementById("inputM3").value;
+    var inputM4 = document.getElementById("inputM4").value;
+    var inputM5 = document.getElementById("inputM5").value;
+    var arrayMeals = [inputM1, inputM2, inputM3, inputM4, inputM5]
+
+    //Table constants
+    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const mealsPerDay = 5;
+    const table = document.createElement('table');
+    const headerRow = document.createElement('tr');
+    table.style.border = "1px solid black";
+    headerRow.style.border = "1px solid black";
+
+    //Generate planner table
+    function createPlanner() {
+
+        //Build the header row
+        for (var i = 0; i < daysOfWeek.length; i++) {
+            var headerCell = document.createElement("th");
+            headerCell.textContent = daysOfWeek[i];
+            headerRow.appendChild(headerCell);
+        }
+        table.appendChild(headerRow);
+
+        //Build the meal rows
+        for (var j = 0; j < mealsPerDay; j++) {
+            var mealRow = document.createElement("tr");
+
+            // Create and append the cells for each day
+            for (var k = 0; k < daysOfWeek.length; k++) {
+            var mealCell = document.createElement("td");
+            mealCell.style.border = "1px solid black";
+            mealCell.textContent = arrayMeals[j];
+            mealRow.appendChild(mealCell);
+            }
+
+            // Append the meal row to the table
+            table.appendChild(mealRow);
+        }
+    }
+
+    //Create the planner
+    createPlanner();
+
+    //Create container and clear button on new page
+    const tableContainer = document.createElement('div');
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear Meals';
+    clearButton.addEventListener('click', function() {
+        arrayMeals = [];
+        createPlanner();
+        tableContainer.removeChild(table);
+        tableContainer.appendChild(table);
+    });
+
+    //Add container to new page
+    tableContainer.appendChild(table);
+    tableContainer.appendChild(clearButton);
+
+    //Open new window
+    flyWindow = window.open('about:blank','myPop');
+    flyWindow.document.write(tableContainer.innerHTML);
 }
